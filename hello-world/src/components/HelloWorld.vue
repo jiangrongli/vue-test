02fd40565a92bfbbd80028727a6f1e3117804b63
jiangrongli <kingdee@YDZH2006C-0005.kingdee.gbl>
2021-03-29 11:30:04 +0800

    vue 测试 并 测试了 npm 包的 使用！

---
+++ hello-world/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import testConsole from "jiangrongli_test";
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  created (){
+    testConsole('第一次测试 npm包使用情况');
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
